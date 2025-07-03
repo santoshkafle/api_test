a69a077f7caa3cf1bf0735f13dd947678ce6eed2
santosh kafle <santoshkafle156@gmail.com>
2025-07-03 17:12:45 +0545

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:api_test/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Api Test',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
